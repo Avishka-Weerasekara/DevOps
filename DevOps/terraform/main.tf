f8c6deff9d09df18d589d34207bc283b7974fd3d
Avishka Weerasekara <Buddhi00weerasekara@gmail.com>
2026-01-27 18:48:56 +0530

    edit

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -1,5 +1,22 @@
+# Get latest Ubuntu 22.04 AMI for eu-north-1
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create EC2 instance
 resource "aws_instance" "devops_ec2" {
-  ami = "ami-0f2b4fc905b0bd1f1"  # Ubuntu 24.04 (eu-north-1)
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
 
