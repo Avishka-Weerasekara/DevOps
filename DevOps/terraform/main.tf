a9c67effec15d076b761a3dd5e29f6bc89dda063
Avishka Weerasekara <Buddhi00weerasekara@gmail.com>
2026-01-30 00:40:21 +0530

    edit

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -1,7 +1,7 @@
-# Get latest Ubuntu 22.04 AMI for eu-north-1
+# Latest Ubuntu 22.04
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical (Ubuntu)
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
@@ -14,11 +14,47 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Create EC2 instance
+# Security Group
+resource "aws_security_group" "devops_sg" {
+  name = "devops-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "devops_ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
+  security_groups = [aws_security_group.devops_sg.name]
+
+  user_data = <<EOF
+#!/bin/bash
+apt update -y
+apt install -y docker.io
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ubuntu
+EOF
 
   tags = {
     Name = "DevOps-EC2"
